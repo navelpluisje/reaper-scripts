1d717044bccb03291e8bbbaab2ed6f904a66d206
Erwin Goossen <erwin@navelpluisje.nl>
2023-11-03 18:15:05 +0100

    Removed some hidden files

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -4,11 +4,11 @@ import { config as envConfig } from 'dotenv';
 import fs from 'fs';
 import path from 'path';
 import chalk from 'chalk';
-import { subtractArgs, copyZoneFile, copyFile } from './utils/index.mjs';
+import { subtractArgs, copyFile } from './utils/index.mjs';
 import fse from 'fs-extra';
 import { zip } from 'zip-a-folder';
 
-const dotEnv = envConfig();
+const _ = envConfig();
 const args = subtractArgs()
 
 if (!process.env.REAPER_PATH) {
